835ab66dd3594d7110c2f701df7de01db9c4c63c
BuildTools <unconfigured@null.spigotmc.org>
2022-11-05 16:23:58 +0100

    shop update

--- stuff/shop.js
+++ stuff/shop.js
@@ -3,44 +3,40 @@ import getCookie from "./getCookie.js";
 import setCookie from "./setCookie.js";
 import update from "./update.js";
 import { playSound, shopButtonClick } from "./audio.js";
+import { formate } from "./textFormating.js";
 
 const btn1 = document.querySelectorAll('.shop > button');
 let powerups = [
     {
         name: '+1',
-        cost: 20,
+        cost: 100,
         add: 1,
-        desc: '+1 power for 20 points'
+    },
+    {
+        name: '+25',
+        cost: 1000,
+        add: 25,
     },
     {
         name: '+50',
-        cost: 100,
-        add: 50,
-        desc: '+50 power for 100 points'
+        cost: 5000,
+        add: 100,
     },
     {
         name: '+100',
-        cost: 1000,
-        add: 100,
-        desc: '+100 power for 1000 points'
-    },
-    {
-        name: '+500',
         cost: 10000,
         add: 500,
-        desc: '+500 power for 10000 points'
     },
     {
-        name: '+9999',
-        cost: 99999,
-        add: 9999,
-        desc: '+9999 power for 99999 points'
+        name: '+10000',
+        cost: 100000,
+        add: 10000,
     },
 
 ]
 
 for(let q = 0; q < powerups.length; q++){
-    btn1[q].innerHTML = `+${powerups[q].add} power </br></br> cost: ${powerups[q].cost}`;
+    btn1[q].innerHTML = `+${formate(powerups[q].add)} power </br></br> cost: ${formate(powerups[q].cost)}`;
     btn1[q].addEventListener('click', () => {
         if(getCookie('points') >= powerups[q].cost){
             const i = Math.round(parseInt(getCookie('points')) - powerups[q].cost); 
